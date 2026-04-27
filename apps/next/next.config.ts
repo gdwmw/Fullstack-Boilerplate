2c75ce635ca858359153f6dd676385007ccd3024
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2026-04-27 22:58:22 +0700

    chore: ⚙️ next config

--- apps/next/next.config.ts
+++ apps/next/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         search: "",
       },
       {
-        hostname: "sztraa.zettara.com",
+        hostname: "elysia.zettara.com",
         pathname: "/**",
         port: "",
         protocol: "https",
