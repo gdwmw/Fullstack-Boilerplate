da44d9b0045d34985d632cae2e314ad24781646c
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2026-04-29 21:12:07 +0700

    feat: 🎉 jotai

--- apps/next/src/context/useGlobalContext.ts
+++ apps/next/src/context/useGlobalContext.ts
@@ -1,10 +1,18 @@
-import { create } from "zustand";
+import { atom, useAtom } from "jotai";
 
 import { IGlobalActions, IGlobalStates } from "./type";
 
-export const useGlobalContext = create<IGlobalActions & IGlobalStates>((set) => ({
-  openA: false,
-  openB: false,
-  setOpenA: (openA: boolean) => set({ openA }),
-  setOpenB: (openB: boolean) => set({ openB }),
-}));
+const openAAtom = atom<IGlobalStates["openA"]>(false);
+const openBAtom = atom<IGlobalStates["openB"]>(false);
+
+export const useGlobalContext = (): IGlobalActions & IGlobalStates => {
+  const [openA, setOpenA] = useAtom(openAAtom);
+  const [openB, setOpenB] = useAtom(openBAtom);
+
+  return {
+    openA,
+    openB,
+    setOpenA,
+    setOpenB,
+  };
+};
