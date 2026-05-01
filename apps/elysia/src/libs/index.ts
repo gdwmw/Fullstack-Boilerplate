39c55b43d1d874eb3ea5a2e92feea833aaf307ba
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2026-05-01 13:23:00 +0700

    refactor: 💡 pino

--- apps/elysia/src/libs/index.ts
+++ apps/elysia/src/libs/index.ts
@@ -1,4 +1,4 @@
 export * from "./jwt";
-export * from "./logger";
+export * from "./pino";
 export * from "./prisma";
 export * from "./redis";
--- apps/elysia/src/libs/pino.ts
+++ apps/elysia/src/libs/pino.ts
@@ -2,14 +2,6 @@ import pino, { type LoggerOptions } from "pino";
 
 const loggerOptions: LoggerOptions = {
   level: process.env.LOG_LEVEL ?? "info",
-  transport: {
-    options: {
-      ignore: "pid,hostname",
-      singleLine: true,
-      translateTime: "SYS:standard",
-    },
-    target: "pino-pretty",
-  },
 };
 
 export const logger = pino(loggerOptions);
