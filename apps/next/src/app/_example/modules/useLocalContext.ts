4051ca30581f1b2411a2d6b463d871cc00b1a369
Gede Dewo Wahyu M.W <programmer3.it@kcngroup.co.id>
2026-05-04 13:46:45 +0700

    build: 📦️ update dependencies

--- apps/next/src/app/_example/modules/useLocalContext.ts
+++ apps/next/src/app/_example/modules/useLocalContext.ts
@@ -1,33 +1,24 @@
 import { atom, useAtom } from "jotai";
 
 interface ILocalState {
-  count: number;
-  query: string;
+  isOpen: boolean;
 }
 
 interface ILocalActions {
-  decrement: () => void;
-  increment: () => void;
-  reset: () => void;
-  setQuery: (value: string) => void;
+  close: () => void;
+  open: () => void;
+  toggle: () => void;
 }
 
-const countAtom = atom<ILocalState["count"]>(0);
-const queryAtom = atom<ILocalState["query"]>("");
+const isOpenAtom = atom<ILocalState["isOpen"]>(false);
 
 export const useLocalContext = (): ILocalActions & ILocalState => {
-  const [count, setCount] = useAtom(countAtom);
-  const [query, setQueryState] = useAtom(queryAtom);
+  const [isOpen, setIsOpen] = useAtom(isOpenAtom);
 
   return {
-    count,
-    decrement: () => setCount((prev) => prev - 1),
-    increment: () => setCount((prev) => prev + 1),
-    query,
-    reset: () => {
-      setCount(0);
-      setQueryState("");
-    },
-    setQuery: (value: string) => setQueryState(value),
+    close: () => setIsOpen(false),
+    isOpen,
+    open: () => setIsOpen(true),
+    toggle: () => setIsOpen((prev) => !prev),
   };
 };
